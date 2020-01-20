ebcd4e943e569e59088b74d1e4c3ffc58df786c4
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-01-20 10:55:17 +0100

    Update scripts.js
    created var repositoryArray and assigned array with different pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-var favoriteFood = 'Indian Dal';
-document.write(favoriteFood);
+//Variable erstellen und Array zuweisen:
+
+var repositoryArray = [
+	{name: 'Nidoqueen', height: 1.3, types:['ground', 'poison']},
+	{name: 'Beedrill', height: 1, types:[bug, poison]},
+	{name: 'Dewgong', height:1.7, types:[ice, water]}
+]
+
