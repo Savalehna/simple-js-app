20e4614b1ee61e27750672caa6fa522cfe8e3c6e
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-01-21 11:46:18 +0100

    Update scripts.js
    1. Changed description in line 1 from german to english.
    2. Added missing simple quotation marks for types in line 5 and 6.
    3. Added semicolon in line 7.
    4. Deleted empty line in line 8.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-//Variable erstellen und Array zuweisen:
+//Create variable and assign array for different pokemon-types
 
 var repositoryArray = [
 	{name: 'Nidoqueen', height: 1.3, types:['ground', 'poison']},
-	{name: 'Beedrill', height: 1, types:[bug, poison]},
-	{name: 'Dewgong', height:1.7, types:[ice, water]}
-]
-
+	{name: 'Beedrill', height: 1, types:['bug', 'poison']},
+	{name: 'Dewgong', height:1.7, types:['ice', 'water']}
+];
