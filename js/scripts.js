fce28ec2a0795a8fc18bc8e48740e736c9452d51
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-01-28 19:11:51 +0100

    Update scripts.js
    1. Deleted unnecessary square brackets in repositoryArray
    2. Inserted space at line 1 and 21
    3. Created loop to iterate each item in repositoryArray
    4. Inserted condition in loop to check if the height is higher than 1.7
    5. Inserted additional string if condition is fulfilled

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
-//Create variable and assign array for different pokemon-types
+// Create variable and assign array for different pokemon-types
 
 var repositoryArray = [
-	{name: 'Nidoqueen', height: 1.3, types:['ground', 'poison']},
-	{name: 'Beedrill', height: 1, types:['bug', 'poison']},
-	{name: 'Dewgong', height:1.7, types:['ice', 'water']}
-];
+  {
+    name: 'Nidoqueen',
+    height: 1.3,
+    types: ['ground', 'poison']
+  },
+  {
+    name: 'Beedrill',
+    height: 1.2,
+    types: ['bug', 'poison']
+  },
+  {
+    name: 'Dewgong',
+    height: 1.7,
+    types: ['ice', 'water']
+  }
+]
+
+// Create loop to iterate each item in repositoryArray
+
+for (var i = 0; i <= 2; i++) {
+  if (repositoryArray[i].height >= 1.7) {
+    document.write('<p>name: ' + repositoryArray[i].name + ' (height: ' + repositoryArray[i].height + ') Wow, that\'s big!</p>')
+  } else {
+    document.write('<p>name: ' + repositoryArray[i].name + ' (height: ' + repositoryArray[i].height + ')</p>')
+  }
+}
