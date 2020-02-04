f3b1431b477f7fa64a6e4033d2889ee1b559158c
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-02-04 12:33:24 +0100

    Update scripts.js
    1. Added new empty line above comment now in line 2
    2. Added document.write in line 20
    3. Changed comment now in line 22
    4. Changed loop:
    4.1 for line 23:  changed "2" in "repositoryArray.length -1"
    4.2 line 25: added main, list-object and bold font-style; deleted <p>
    4.3 line 27: added main, list-object and bold font-style; deleted <p>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-// Create variable and assign array for different pokemon-types
 
+// Create variable and assign array for different pokemon-types
 var repositoryArray = [
   {
     name: 'Nidoqueen',
@@ -17,13 +17,13 @@ var repositoryArray = [
     types: ['ice', 'water']
   }
 ]
+document.write('<h1>Pokemons:</h1>')
 
-// Create loop to iterate each item in repositoryArray
-
-for (var i = 0; i <= 2; i++) {
+// Create loop to iterate each item in repositoryArray; Check the height and show a message if height is above 1.7 --> "Wow, that's big!""
+for (var i = 0; i <= repositoryArray.length - 1; i++) {
   if (repositoryArray[i].height >= 1.7) {
-    document.write('<p>name: ' + repositoryArray[i].name + ' (height: ' + repositoryArray[i].height + ') Wow, that\'s big!</p>')
+    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ') Wow, that\'s big!</li></main>')
   } else {
-    document.write('<p>name: ' + repositoryArray[i].name + ' (height: ' + repositoryArray[i].height + ')</p>')
+    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ')</li></main>')
   }
 }
