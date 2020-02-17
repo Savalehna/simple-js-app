0cec6a3a80255c20f906a8811cf582ca58aa0230
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-02-17 10:59:43 +0100

    Update scripts.js
    1. Inserted blanks in line7,12 and 17
    2. Inserted empty line in line 20
    3. Changed for-if-else-loop from line 23 to 29 in comment
    4. Added forEach function in line 32 to 35 to loop over the Pokémon in the repositoryArray in order to print the details of each one.

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,32 @@ var repositoryArray = [
   {
     name: 'Nidoqueen',
     height: 1.3,
-    types: ['ground', 'poison']
+    types: ['ground', ' poison']
   },
   {
     name: 'Beedrill',
     height: 1.2,
-    types: ['bug', 'poison']
+    types: ['bug', ' poison']
   },
   {
     name: 'Dewgong',
     height: 1.7,
-    types: ['ice', 'water']
+    types: ['ice', ' water']
   }
 ]
+
 document.write('<h1>Pokemons:</h1>')
 
 // Create loop to iterate each item in repositoryArray; Check the height and show a message if height is above 1.7 --> "Wow, that's big!""
-for (var i = 0; i <= repositoryArray.length - 1; i++) {
-  if (repositoryArray[i].height >= 1.7) {
-    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ') Wow, that\'s big!</li></main>')
-  } else {
-    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ')</li></main>')
-  }
-}
+// for (var i = 0; i <= repositoryArray.length - 1; i++) {
+//  if (repositoryArray[i].height >= 1.7) {
+//    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ') Wow, that\'s big!</li></main>')
+//  } else {
+//    document.write('<main><li><b>Name: </b>' + repositoryArray[i].name + ' (Height: ' + repositoryArray[i].height + ')</li></main>')
+//  }
+// }
+
+ // ForEach function to loop over the Pokémon in the repositoryArray in order to print the details of each one.
+repositoryArray.forEach(function(item, index, array) {
+document.write('<b>Name: </b>' + item.name + ', Height: ' + item.height + ', Types: ' + item.types + '<div></div>')
+});
