e6ad26004f9b331ed9e5ddf93a31a16b16866002
Savalehna <52879175+Savalehna@users.noreply.github.com>
2020-03-09 11:04:56 +0100

    Update scripts.js
    1. Changed comments in line 1, 2, 20 and 25
    2. Added new function 'listItem' with 'button' to show each pokemen item and to execute function 'showDetails' when the 'button' is clicked in line 30 -52
    3. Thereofre the 'return' function moves from line 31 to 53 + 54
    4. Therefore 'getAll' moved from line 33 to 56
    5. Added 'addListItem' in line 57
    6. Deleted line 37 + 38
    7. Added new variable '$pokemonList' in line 61 + 62 for the the new forEach loop block
    5. Deleted unnecessary forEach function in line 40 - 43
    6. Added new forEach function in line 64 - 69

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-
-// Create variable and assign array for different pokemon-types.
-// Wrap repository array in an IIFE () () to avoid accidentally accessing the global state.
+// Create a variable and assign array for different pokemon-types.
+// Wrap repository array in an IIFE () () to avoid accidentally accessing the global state:
 var pokemonRepository = (function () {
   var repository = [{
     name: 'Nidoqueen',
@@ -18,26 +17,53 @@ var pokemonRepository = (function () {
     types: ['ice', ' water']
   }];
 
-// Function to add new Pokemon data
+// Function to add new pokemon data (The parameter pokemon represents a single pokemon):
   function add(pokemon) {
     repository.push(pokemon);
   }
 
-// Function to pull all Pokemon data
+// Function to pull all pokemon data:
   function getAll() {
     return repository;
   }
 
-  return {
+// Function for adding new listItem:
+  function addListItem(pokemon){
+// Create a new li-element that contains a button for each pokemon:
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+// Appends the button to the list item as its child:
+      listItem.appendChild(button);
+// Appends the list item to the unordered list as its child:
+    $pokemonList.appendChild(listItem);
+// The button shows the name from the current pokemon:
+    button.innerText = pokemon.name;
+// Give the button a custom button style from styles.css to overwrite default styling:
+    button.classList.add('button');
+// Add a function to the button which is executed when the button is clicked:
+    button.addEventListener('click', function (){
+      showDetails(pokemon)
+      })
+    }
+// Function which will be executed when the button is clicked in function addListItem.
+// Log the details of the current pokemon in the console:
+    function showDetails(pokemon){
+      console.log(pokemon)
+    }
+// Returns the values wich can be accessed to outside the IIFE:
+  return{
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-// Adds a single item (Pokemon)
-pokemonRepository.add({ name: 'Pikachu' });
+// Creates a variable above the forEach loop block and assigns it to the ul element:
+var $pokemonList = document.querySelector('ul');
 
-//Creates list of Pokemon:
-pokemonRepository.getAll().forEach(function(pokemon) {
-document.write('<b>Name: </b>' + pokemon.name + '<div></div>');
-});
+// The forEach block, calls the function addListItem inside the IIFE
+// and passes the pokemon returned in the forEach block to the addListItem function call.
+// It returns a pokemon in each iteration:
+pokemonRepository.getAll().forEach(function(currentItem){
+  pokemonRepository.addListItem(currentItem);
+})
